b78b3417095120a7b9ceb6a23592d33f04cec8ca
jwj <jwj@macmini>
2023-06-08 16:58:09 +0800

    获取编辑器 .gj 文件的文本

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import * as path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log(
@@ -8,26 +9,33 @@ export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     "vscode-guji-preview.preview",
     () => {
-      // 1. 获取编辑器 .gj 文件的文档内容
+      // 1. 获取编辑器 .gj 文件的文本
+      const editor = vscode.window.activeTextEditor;
 
-      // 2. 将 .gj 文档转为 html
+      if (!editor) {
+        return;
+      }
+      const text = editor.document.getText();
+      const previewTitle = `预览 ${path.basename(editor.document.fileName)}`;
+
+      // 2. 将 .gj 文本转为 html
       const html = `<!DOCTYPE html>
-	  <html lang="en">
-	  <head>
-		  <meta charset="UTF-8">
-		  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-		  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-		  <title>预览窗口</title>
-	  </head>
-	  <body>
-		  预览内容
-	  </body>
-	  </html>`;
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>${previewTitle}</title>
+        </head>
+        <body>
+          ${text}
+        </body>
+        </html>`;
 
       // 3. 创建 Webview 预览窗口
       const panel = vscode.window.createWebviewPanel(
         "guji-preview",
-        "预览",
+        previewTitle,
         vscode.ViewColumn.Beside
       );
 
