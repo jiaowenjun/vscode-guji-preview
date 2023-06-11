641c1e300b8174202b1f1b0313442430b89e5df1
jwj <jwj@mba>
2023-06-11 15:13:12 +0800

    引入 react 模块

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,5 +1,9 @@
 import { test, expect } from "vitest";
+import React from "react";
+import { renderToString } from "react-dom/server";
 
 test("main", () => {
   expect(true).toBe(true);
+  const line = React.createElement("div", { className: "line" }, "line1");
+  console.log(renderToString(line));
 });
